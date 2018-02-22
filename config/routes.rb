83222536b5e6c7646a62b39bf50913df4d4e96d2
Geoffrey Cheah <geoffcheah@gmail.com>
2018-02-22 14:57:46 +0000

    added index page and show link

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :cocktails, only: [:index, :show, :new, :create]
 
+  root to: 'cocktails#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
